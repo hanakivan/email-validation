34d970de88696fa27b4ad261a9845b0aac067277
Ivan #1 <ivan_1@IvanHanak.local>
2020-07-09 21:50:31 +0200

    Added sources

---
+++ src/index.test.js
@@ -0,0 +1,31 @@
+import validateEmail from './index';
+
+test('just a string', () => {
+    expect(validateEmail("my name is Pensy")).toBeFalsy()
+});
+
+test("tld missing", () => {
+    expect(validateEmail("gabriel@gmail")).toBeFalsy()
+});
+
+test("tld without a preceeding dot", () => {
+    expect(validateEmail("gabriel@gmailcom")).toBeFalsy()
+});
+
+test("no at sign", () => {
+    expect(validateEmail("gabriel.gmail.com")).toBeFalsy()
+});
+
+
+test("valid corporate email", () => {
+    expect(validateEmail("John.Doe@ibm.com")).toBeTruthy()
+});
+test(".apartments tld", () => {
+    expect(validateEmail("John.Doe@airbnb.apartments")).toBeTruthy()
+});
+test(".cancerresearch tld", () => {
+    expect(validateEmail("John.Doe@foundation.cancerresearch")).toBeTruthy()
+});
+test(".porn tld", () => {
+    expect(validateEmail("John.Doe@myhomemade.porn")).toBeTruthy()
+});
